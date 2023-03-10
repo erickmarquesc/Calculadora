53c5748abe3d0e25a1ef9ec639fcfc0c48f9f029
Erick Marques <erick.marquesc@hotmail.com>
2023-03-10 13:48:11 -0300

    feat: :zap: add context

--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -7,42 +7,83 @@ import minus from "../../assets/minus.svg";
 import plusminus from "../../assets/plusminus.svg";
 import result from "../../assets/result.svg";
 import { Button } from "./Button/styles";
+import { useCalcResult } from "../../context/CalcContext";
 
 export function Keyboard() {
+
+  
+  const { handleSetOperator, ValueTerm, calcResult} = useCalcResult();
+
   return (
     <KeyboardContainer>
-      <Button className="secondary">CE</Button>
-      <Button>C</Button>
+      <Button className="secondary"
+        onClick={() => handleSetOperator('CE')}
+      >CE</Button>
+      <Button
+        onClick={() => handleSetOperator('C')}
+      >C</Button>
       <Button>
         <img src={percent} alt="Percent" />
       </Button>
-      <Button className="tertiary">
+      <Button className="tertiary"
+        onClick={() => handleSetOperator('/')}
+      >
         <img src={divide} alt="Divide" />
       </Button>
-      <Button>7</Button>
-      <Button>8</Button>
-      <Button>9</Button>
-      <Button className="tertiary">
+      <Button
+        onClick={() => ValueTerm(7)}
+      >7</Button>
+      <Button
+        onClick={() => ValueTerm(8)}
+      >8</Button>
+      <Button
+        onClick={() => ValueTerm(9)}
+      >9</Button>
+      <Button className="tertiary"
+        onClick={() => handleSetOperator('*')}
+      >
         <img src={multiplication} alt="Multiplication" />
       </Button>
-      <Button>4</Button>
-      <Button>5</Button>
-      <Button>6</Button>
-      <Button className="tertiary">
+      <Button
+        onClick={() => ValueTerm(4)}
+      >4</Button>
+      <Button
+        onClick={() => ValueTerm(5)}
+      >5</Button>
+      <Button
+        onClick={() => ValueTerm(6)}
+      >6</Button>
+      <Button className="tertiary"
+        onClick={() => handleSetOperator('-')}
+      >
         <img src={minus} alt="Minus" />
       </Button>
-      <Button>1</Button>
-      <Button>2</Button>
-      <Button>3</Button>
-      <Button className="tertiary">
+      <Button
+        onClick={() => ValueTerm(1)}
+      >1</Button>
+      <Button
+        onClick={() => ValueTerm(2)}
+      >2</Button>
+      <Button
+        onClick={() => ValueTerm(3)}
+      >3</Button>
+      <Button className="tertiary"
+        onClick={() => handleSetOperator('+')}
+      >
         <img src={plus} alt="Plus" />
       </Button>
-      <Button>
+      <Button
+        onClick={() => handleSetOperator('x/-')}
+      >
         <img src={plusminus} alt="PlusMinus" />
       </Button>
-      <Button>0</Button>
+      <Button
+        onClick={() => ValueTerm(0)}
+      >0</Button>
       <Button>,</Button>
-      <Button className="quartiary">
+      <Button className="quartiary"
+        onClick={() => calcResult()}
+      >
         <img src={result} alt="Equals" />
       </Button>
     </KeyboardContainer>
