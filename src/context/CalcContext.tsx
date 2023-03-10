5ffe15ac3ef45f2a29b9fd3adca7eb7a301d702a
Erick Marques <erick.marquesc@hotmail.com>
2023-03-10 13:47:21 -0300

    feat: :sparkles: created a new context

---
+++ src/context/CalcContext.tsx
@@ -0,0 +1,80 @@
+import { createContext, ReactNode, useContext, useState } from "react"
+
+export interface ICalcContextType {
+
+  calcResult: () => void;
+  handleSetOperator: (operação: string) => void;
+  ValueTerm: (value: number) => void;
+  valueFirstTerm: number;
+  valueSecondTerm: number;
+  result: number;
+  operator: string;
+};
+
+export interface ICalcProviderProps {
+  children: ReactNode;
+};
+
+export const CalcContext = createContext({} as ICalcContextType);
+export function CalcContextProvider({ children }: ICalcProviderProps) {
+  
+  const [valueFirstTerm, setValueFirstTerm] = useState(0);
+  const [valueSecondTerm, setValueSecondTerm] = useState(0);
+  const [operator, setOperator] = useState("#");
+  const [result, setResult] = useState(0);
+
+  function ValueTerm(value: number) {
+    if (operator === '#') {
+      return setValueFirstTerm(value);
+    } else {
+      return setValueSecondTerm(value);
+    }
+  };
+
+  function handleSetOperator(operação: string) {
+    setOperator(operação);
+  };
+
+  function calcResult() {
+
+    switch (operator) {
+      case '+':
+        setResult(valueFirstTerm + valueSecondTerm)
+        break;
+      case '-':
+        setResult(valueFirstTerm - valueSecondTerm)
+        break;
+      case '*':
+        setResult(valueFirstTerm * valueSecondTerm)
+        break;
+      case '/':
+        setResult(valueFirstTerm / valueSecondTerm)
+        break;
+      /* case '+/-':
+        setResult(valueFirstTerm + valueSecondTerm)
+        break;
+      case '%':
+        setResult(valueFirstTerm + valueSecondTerm)
+        break; */
+    }
+  };
+
+  return (
+    <CalcContext.Provider value={{
+      calcResult,
+      ValueTerm,
+      handleSetOperator,
+      valueFirstTerm,
+      valueSecondTerm,
+      operator,
+      result,
+    }}>
+      {children}
+    </CalcContext.Provider>
+  )
+}
+
+// HOOK
+export function useCalcResult() {
+  return useContext(CalcContext);
+};
