c2e599a764ded3c2b6be77c27a09e904b80ac85a
Erick Marques <erick.marquesc@hotmail.com>
2023-03-10 13:47:49 -0300

    refactor: :zap: add context

--- src/components/Display/index.tsx
+++ src/components/Display/index.tsx
@@ -1,13 +1,22 @@
 import { DisplayContainer, ResultSection } from "./styles";
+import { useCalcResult } from "../../context/CalcContext";
 import equals from "../../assets/equals.svg";
 
 export function Display() {
+
+  const { valueFirstTerm, valueSecondTerm, result, operator } = useCalcResult();
+
   return (
     <DisplayContainer>
-      <div id="last-calc">1 + 1</div>
+      <div id="last-calc">
+        {valueFirstTerm}
+        <label>
+          {operator !== "#" ? operator : '+'}
+        </label>
+        {valueSecondTerm}</div>
       <ResultSection>
         <img src={equals} alt="sinal de igual" />
-        <span>2</span>
+        <span>{result}</span>
       </ResultSection>
     </DisplayContainer>
   );
--- src/components/Display/styles.ts
+++ src/components/Display/styles.ts
@@ -3,6 +3,17 @@ import styled from "styled-components";
 export const DisplayContainer = styled.div`
   margin-top: 54px;
   padding-inline: 54px;
+
+  #last-calc {
+  text-align: right;
+  font-size: 20px;
+  line-height: 140%;
+  color: #6b6b6b;
+
+  label{
+    margin: 5px;
+  }
+}
 `; 
 
 export const ResultSection = styled.div`
