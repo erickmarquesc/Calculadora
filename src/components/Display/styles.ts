64bc2b7512607dc95da77acbc3bd6608d83bd20c
Erick Marques <erick.marquesc@hotmail.com>
2023-04-19 20:01:39 -0300

    style: :lipstick: add and update UI

--- src/components/Display/styles.ts
+++ src/components/Display/styles.ts
@@ -5,26 +5,26 @@ export const DisplayContainer = styled.div`
   padding-inline: 54px;
 
   #last-calc {
-  padding-top: 54px;
-  text-align: right;
-  font-size: 20px;
-  line-height: 140%;
-  color: #6b6b6b;
+    color: #6b6b6b;
+    font-size: 20px;
+    padding-top: 54px;
+    text-align: right;
+    line-height: 140%;
 
-  label{
-    margin: 5px;
-  }
-}
-`; 
+    label{
+      margin: 5px;
+    };
+  };
+`;
 
 export const ResultSection = styled.div`
-  margin-top: 8px;
   display: flex;
+  margin-top: 8px;
   justify-content: space-between;
 
   span {
-  font-size: 36px;
-  line-height: 140%;
-  color: #ebebeb;
-}
+    color: #ebebeb;
+    font-size: 36px;
+    line-height: 140%;
+  };
 `;
--- src/components/Keyboard/styles.ts
+++ src/components/Keyboard/styles.ts
@@ -2,9 +2,20 @@ import styled from "styled-components";
 
 export const KeyboardContainer = styled.div`
   gap: 12px;
-  display: grid;
+  display: flex;
   padding: 26px 32px 32px;
-  grid-template-columns: repeat(4, 1fr);
+  flex-direction: column;
+
+  .operatorrow{
+    gap: 12px;
+    display: flex;
+    flex-direction: row;
+  }
+  .operatorcolumn{
+    gap: 12px;
+    display: flex;
+    flex-direction: column;
+  }
 
   .secondary {
     color: #975dfa;
@@ -22,4 +33,15 @@ export const KeyboardContainer = styled.div`
     background: linear-gradient(180deg, rgba(0, 0, 0, 0.15) 0%, rgba(255, 255, 255, 0.05) 100%), #7F45E2;
     box-shadow: 0px 11px 7px rgba(0, 0, 0, 0.01), 0px 7px 7px rgba(0, 0, 0, 0.04), 0px 4px 6px rgba(0, 0, 0, 0.1), 0px 2px 4px rgba(0, 0, 0, 0.26), 0px 0px 2px rgba(0, 0, 0, 0.29), inset 0px 2px 3px rgba(255, 255, 255, 0.1);
   }
+`;
+
+export const KeyboardContainerTerms = styled.div`
+  gap: 12px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`;
+
+export const KeyboardContent = styled.div`
+  gap: 12px;
+  display: flex;
 `;
